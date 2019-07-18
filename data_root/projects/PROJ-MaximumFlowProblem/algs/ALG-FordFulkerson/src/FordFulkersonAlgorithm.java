e8832f49ac5ca96103a446e0d36eba4c30289431
Žiga Kokelj <ziga.kokelj@gmail.com>
2019-07-18 17:51:29 +0200

    FordFulkerson algorithm start

--- data_root/projects/PROJ-MaximumFlowProblem/algs/ALG-FordFulkerson/src/FordFulkersonAlgorithm.java
+++ data_root/projects/PROJ-MaximumFlowProblem/algs/ALG-FordFulkerson/src/FordFulkersonAlgorithm.java
@@ -1,15 +1,23 @@
 /**
  *
- * @author ...
+ * @author Ziga Kokelj
  */
+//TODO: Import Graph data structure defined in MaximumFlowProblemInput
+
 public class FordFulkersonAlgorithm extends MaximumFlowProblemAbsAlgorithm {
 
   @Override
   protected MaximumFlowProblemOutput execute(MaximumFlowProblemInput input) {
     
-    // TODO: implement the algorithm that uses the input and generates the output
+    int n = input.g.getVertexCount();
+    Graph g = input.g;
+    int a = input.a;
+    int b = input.b;
+    int maxFlow = 0;
+
+    //TODO: Ford-Fulkerson algorithm that calculates maxFlow
     
-    MaximumFlowProblemOutput output = new MaximumFlowProblemOutput(/* TODO: add parameters for constructor */);    
+    MaximumFlowProblemOutput output = new MaximumFlowProblemOutput(maxFlow);    
     return output;
   }
 
